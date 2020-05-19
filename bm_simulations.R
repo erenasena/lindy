db7354f5df09606d037355b57e4c5e9fe28922ec
erenasena <erenasena13@gmail.com>
2020-05-19 16:09:20 +0200

    Update bm_simulations.R

--- bm_simulations.R
+++ bm_simulations.R
@@ -215,26 +215,26 @@ events <- function(x, nsim, n){
 
 ## Parallel runs 
 f <- function(i){ # specify the desired function and parameter values here
-  my_gbm(nsim = 1, t0 = 0, t = 1, n = 104, X0 = 100, mu = -1, sigma = 1, L = 35, R = 100000000) 
+  my_gbm(nsim = 1, t0 = 0, t = 1, n = 100, X0 = 100, mu = -1, sigma = 1, L = 75, R = 100000000) 
   #my_abm(nsim = 1, t0 = 0, t = 1, n = 1000, X0 = 1, mu = -1, sigma = 1, L = 0.65, R = 1.01)
 }
 
 set.seed(1)
-res <- mclapply(X = 1:178, f, mc.cores = 8, mc.set.seed = TRUE)
+res <- mclapply(X = 1:250, f, mc.cores = 8, mc.set.seed = TRUE)
 
-v <- values(x = res, nsim = 178, n = 104) # indexing the BM values 
+v <- values(x = res, nsim = 250, n = 100) # indexing the BM values 
 m_val <- v[[1]] # BM values in a matrix (goes into the plotting function)
 df_val <- v[[2]] # BM values in a data frame
 
-t <- times(x = res, nsim = 178, n = 104) # indexing the hitting times 
+t <- times(x = res, nsim = 250, n = 100) # indexing the hitting times 
 m_times <- t[[1]] # in a matrix (for histograms)
 df_times <- t[[2]] # in a data frame 
 
-e <- events(x = res, nsim = 178, n = 104)
+e <- events(x = res, nsim = 250, n = 100)
 m_event <- e[[1]] # in a matrix
 df_event <- e[[2]]
 
-p <- bmplot(x = m_val, nsim = 178, n = 104, L = -100, R = -100, ylim = c(min(m_val), max(m_val)), # Define the range of the y-axis  
+p <- bmplot(x = m_val, nsim = 250, n = 100, L = 90, R = 0, ylim = c(min(m_val), max(m_val)), # Define the range of the y-axis  
             title = "Brownian motion with an absorbing barrier")
 print(p)
 
@@ -242,103 +242,13 @@ print(p)
 hist(m_times, breaks = 100, xlim = c(0, 110), main = 'GBM with an absorbing barrier')
 legend(x = "center", legend = c('mu = -1', 'sigma = 1', 'L = 90', 'R = -100'))
 
-### Getting the hazard functions 
-surv_data <- data.frame(Time = m_times, Event = m_event, row.names = paste0("Sim", 1:nrow(m_times), ""))
-surv_object <- Surv(time = m_times, event = m_event) 
-fit <- bshazard::bshazard(surv_object ~ 1, data = surv_data)
-hazard <- plot(fit$time, fit$hazard, xlab='Time', ylab = 'Hazard Rate', type = 'l', xlim = c(0, 110), ylim = c(min(fit$haz), max(fit$haz)))
-legend(x = "topright", legend = c('mu = -1', 'sigma = 1', 'L = 90', 'R = 125'))
-
-### Hazard function trials
-set.seed(1)
-exps <- rexp(n = 10000, rate = 0.00001)
-expsurv <- bshazard::bshazard(Surv(exps) ~ 1)
-plot(expsurv$time, expsurv$hazard, type = 'l', xlim = c(0, 10000), 
-                                                        ylim = c(min(expsurv$haz), max(expsurv$haz)))
-hist(exps)
-cums <- cumsum(expsurv$hazard)
-plot(expsurv$time, cums, type = 'l', xlim = c(0, 10000), ylim = c(min(cums), max(cums)))
-
-set.seed(1)
-pars <- VGAM::rpareto(n = 10000, scale = 1000, shape = 1.7)
-parsurv <- bshazard::bshazard(Surv(pars) ~ 1)
-plot(parsurv$time, parsurv$hazard, type = 'l', xlim = c(min(parsurv$time), max(parsurv$time)), 
-     ylim = c(min(parsurv$haz), max(parsurv$haz)))
-hist(pars)
-cums <- cumsum(parsurv$hazard)
-plot(parsurv$time, cums, type = 'l', xlim = c(0, 10000), ylim = c(min(cums), max(cums)))
 
 
 
-# The hazard function of Pareto
-library(VGAM)
-hazard <- function(x, a, b){
-  fx <- dpareto(x, shape = a, scale = b)
-  dx <- ppareto(x, shape = a, scale = b)
-  sx <- 1-dx
-  hx <- fx/sx
-  return(hx)
-}
-plot(hazard(x = 1:100, a = 2, b = 1), type = "l")
-
-# The hazard function of exponential 
-eh <- function(x, b){
-  for(i in 1:100)
-    x[i] <- 1/b
-  return(x)
-}
-evalues <- eh(x = 1:100, b = 1)
-plot(evalues, type = "l")
-
-# The hazard function of Weibull 
-wh <- function(a, b, t){
-  h <- (b/a)*((t/a)^(b-1))
-  return(h)
-}
-wvalues <- wh(a = 1, b = 2, t = 1:100)
-plot(wvalues, type = 'l')
-
-## bshazard  
-library(bshazard)
-
-# Pareto: should be decreasing and it is
-set.seed(1234)
-prt <- VGAM::rpareto(n = 1000, shape = 2, scale = 1)
-fit_1 <- bshazard(Surv(prt) ~ 1)
-plot(x = fit_1$time, y = fit_1$hazard, ylim =c(min(fit_1$haz), max(fit_1$haz)), 
-     xlab = "Time", ylab = "Hazard", type = 'l')
-
-# Exponential: should be constant but it is not
-set.seed(1234)
-exps <- rexp(n = 100, 1)
-fit_2 <- bshazard(Surv(exps) ~ 1)
-plot(x = fit_2$time, y = fit_2$hazard, ylim = c(min(fit_2$haz), max(fit_2$haz)), 
-     xlab = "Time", ylab = "Hazard", type = 'l')
-
-# Weibull: should be decreasing for shape < 1, constant for shape = 1, increasing for shape > 1
-# except for constant hazard, it's all good 
-set.seed(1234)
-wei <- rweibull(n = 1000, shape = 4, scale = 1)
-fit_3 <- bshazard(Surv(wei) ~ 1)
-plot(x = fit_3$time, fit_3$hazard, ylim = c(min(fit_3$haz), max(fit_3$haz)), 
-     xlab = "Time", ylab = "Hazard", type = 'l')
 
 
 
-x <- seq(from = 0.1, to = 0.5, by = 0.1)
 
-f <- function(y){
-  for(i in 1:length(y)){
-    y[i] <- rnorm(n = 1, mean = 0, sd =1)
-    if(y[i] <= 0.3){
-      y[i] <- y[i]
-      } else {
-      Recall(y = x)
-    }
-  }
-  return(y)
-}
-f(y = x)
 
 
 
