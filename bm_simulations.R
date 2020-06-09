c65a139b3635b06ee3319d4d6cd8b6c1421bf367
erenasena <erenasena13@gmail.com>
2020-06-09 18:36:09 +0200

    Update bm_simulations.R

--- bm_simulations.R
+++ bm_simulations.R
@@ -1,4 +1,9 @@
-# The necessary libraries
+### Outline: 
+# 1-Brownian motion simulations
+# 2-Describing the hitting time distributions
+# 3-Computing hazard functions and conditional survival (the Lindy effect) 
+
+### The necessary libraries
 library(dplyr) # don't know why 
 library(parallel) # to run the parallel simulations 
 library(ggplot2) # to plot the Brownian motion simulations
@@ -6,6 +11,7 @@ library(survival) # survival analysis
 library(survminer) # to plot the survival curves 
 library(dynpred) # to compute conditional survival 
 library(qualityTools) # for the Q-Q plots 
+library(evir) # for the mean excess plot 
 
 ### The Brownian motion functions 
 
@@ -141,40 +147,59 @@ events <- function(x, nsim, n){
 
 ## Parallel runs 
 f <- function(i){ # specify the desired function and parameter values here
-  my_gbm(nsim = 1, t0 = 0, t = 1, n = 1000, X0 = 100, mu = -1, sigma = 1, L = 90, R = 11000000000) 
+  my_gbm(nsim = 1, t0 = 0, t = 1, n = 10000, X0 = 100, mu = -1, sigma = 1, L = 90, R = 11000000000) 
   #my_abm(nsim = 1, t0 = 0, t = 1, n = 1000, X0 = 0, mu = 0, sigma = 1, L = -0.1, R = 10000000)
 }
 
 set.seed(1)
 res <- mclapply(X = 1:8973, f, mc.cores = 8, mc.set.seed = TRUE)
 
-v <- values(x = res, nsim = 8973, n = 1000) # indexing the BM values 
+v <- values(x = res, nsim = 8973, n = 10000) # indexing the BM values 
 m_val <- v[[1]] # BM values in a matrix (goes into the plotting function)
 df_val <- v[[2]] # BM values in a data frame
 
-t <- times(x = res, nsim = 8973, n = 1000) # indexing the hitting times 
+t <- times(x = res, nsim = 8973, n = 10000) # indexing the hitting times 
 m_times <- t[[1]] # in a matrix (for histograms)
 df_times <- t[[2]] # in a data frame 
 
-e <- events(x = res, nsim = 8973, n = 1000)
+e <- events(x = res, nsim = 8973, n = 10000)
 m_event <- e[[1]] 
 df_event <- e[[2]]
 
 data <- cbind(df_times, df_event)
 
-## Guessing the distribution 
+### Describing the hitting time distribution
 
-# Histogram of the hitting times
-hist(data$`Hitting time`, breaks = 10, xlim = c(0, 1010), main = 'GBM with an absorbing barrier')
-legend(x = "center", legend = c('mu = -1', 'sigma = 1', 'L = 90', 'R = -100'))
+# Maximum to sum (MS) plot; converges to 0 if the moments are defined. Does not make sense to get 
+# moments if they are infinite. So checking this first. 
+ms <- function(x, p){ # x is the hitting times vector, p is the moment for which you want to test 
+  ms <- numeric(length = length(x))
+  n <- numeric(length = length(x))
+  for(i in 1:length(x)){
+    max <- max(x[1:i] ^ p)
+    sum <- sum(x[1:i] ^ p)
+    ms[i] <- max / sum
+    n[i] <- i
+  }
+  return(list("MS" = ms, "n" = n))
+}
 
-# Descriptive statistics of the hitting times
-quantile(data$`Hitting time`) # the sample quantiles
+ratio <- ms(x = data$`Hitting time`, p = 4) # if kurtosis is defined, the rest is defined also 
+plot(x = ratio$n, y = ratio$MS, type = 'l', xlab = "Number of values", ylab = "Max / sum ratio")
+
+# Descriptive statistics of the hitting times; moments are defined so we can get mean and sd 
+mean <- mean(data$`Hitting time`)
+sd <- sd(data$`Hitting time`)
+quantile(data$`Hitting time`) 
 ext <- m_times[which(data$`Hitting time` > 250)] # the extreme values 
 length(ext) / length(data$`Hitting time`) # what proportion of data are larger than a certain value 
 sum(ext) / sum(data$`Hitting time`) # what proportion of the sum they make
 
-# Q-Q Plot to check exponentiality (if concave, there might be heavy tailedness)
+# Histogram of the hitting times
+hist(data$`Hitting time`, breaks = 100, xlim = c(0, 10010), main = 'GBM with an absorbing barrier')
+legend(x = "center", legend = c('mu = -1', 'sigma = 1', 'L = 90', 'R = -100'))
+
+# Q-Q Plot to check exponentiality (if linear, thin tails, if concave, there may be heavy tailedness)
 hittings <- sort(data$`Hitting time`) # sort the data
 p <- ppoints(hittings, length(hittings)) # get the probabilities of the data
 s <- quantile(x = hittings, p = p) # sample quantiles
@@ -182,25 +207,141 @@ q <- qexp(p = p) # exponential quantiles
 qqplot(x = s, y = q, xlab = "Sample quantiles", ylab = "Theoretical quantiles", 
        main = "Exponential QQ Plot")
 
-
 qqPlot(x = data$`Hitting time`, y = "exponential", xlab = "Sample quantiles", 
        ylab = "Theoretical quantiles", main = "Exponential Q-Q Plot") # shorter way with confidence bands
 
-
-# Zipf plot to check for power law decay 
+# Zipf / log-log plot to check for power law decay (linearity indicates power law)
 hittings <- sort(data$`Hitting time`) # sort the data
 fit <- ecdf(hittings) # empirical cumulative distribution function
 s <- 1 - fit(hittings) # empirical survival function  
 logs <- log(s) # the log of the survival function 
 logx <- log(hittings) # the log of the sorted failure times 
 
-plot(x = logx, y = logs, # if linear, indication of power law 
-     xlab = "log(failure times)", ylab = "log(survival function)", 
-     main = "The Zipf Plot") 
+plot(x = logx, y = logs, xlab = "log(failure times)", ylab = "log(survival function)", main = "The Zipf Plot") 
 
-### Survival curves and fitting the model 
-surv_data <- data.frame(Time = m_times, Event = m_event, row.names = paste0("Sim", 1:nrow(m_times), ""))
-surv_object <- Surv(time = m_times, event = m_event) 
+zipfplot <- function (data, type = "plot", title = TRUE){
+  # type should be equal to ’points’ if you want to add the
+  # Zipf Plot to an existing graph
+  # With other strings or no string a new graph is created.
+  # If title is set to be F, the title of the plot is not given.
+  # This can be useful when embedding the Zipf plot into other
+  # plots.
+  data <- sort(as.numeric(data)) #sorting data 
+  y <- 1 - ppoints(data) #computing 1-F(x)
+  if (type == "points") {
+    points(data, y, xlog=T, ylog=T, xlab = "x on log scale",
+           ylab = "1-F(x) on log scale")
+    }
+  else if(title==F){
+    plot(data, y, log="xy", xlab = "x on log scale", ylab = "1-F(x) on log scale")
+    }
+  else{
+    plot(data, y, log="xy", xlab = "x on log scale", ylab = "1-F(x) on log scale", main="Zipf Plot")
+    }
+}
+
+zipfplot(data = data$`Hitting time`)
+
+# Mean excess (ME) plot (linearity indicates power law, concavity lognorm, constant exp, decreasing norm)
+evir::meplot(sort(data$`Hitting time`)) 
+VGAM::meplot(sort(data$`Hitting time`)) # gives confidence bands
+
+meplot <- function(data, cut = 5){
+  # In cut you can specify the number of maxima you want to exclude. # The standard value is 5
+  data <- sort(as.numeric(data)); n = length(data);
+  mex <- c();
+  for (i in 1:n){
+    mex[i] <- mean(data[data > data[i]]) - data[i];
+  }
+  data_out <- data[1:(n - cut)];
+  mex_out <- mex[1:(n - cut)];
+  plot(data_out, mex_out, xlab = "Threshold u", ylab = "Mean Excess e(u)", main = "Mean Excess Plot (Meplot)")
+}
+
+meplot(data = data$`Hitting time`)
+
+# Discriminant moment ratio plot; this is supposed to discriminate the distribution but is a bit faulty
+moment_plot <- function(data){
+  # "data" is a vector containing the sample data
+  ############################################## ############################################## # CV and Skewness functions coefvar=function(data){
+  # CV and Skewness functions 
+  coefvar=function(data){
+    CV=sd(data)/mean(data)
+    CV}
+  skewness=function(data) {
+    m_3 <- mean((data-mean(data))^3) 
+    skew <- m_3/(sd(data)^3)
+    skew}
+############################################## ############################################## # Computation of CV and Skewness
+# CV
+  CV=coefvar(data); 
+  # Skewness 
+  skew=skewness(data) 
+  # Rule of Thumb
+  if (CV<0 | skew <0.15){print("Possibly neither nor lognormal. Thin tails."); stop}
+############################################## # Preparation of the plot ############################################## # Paretian Area
+  ############################################## # Paretian Area
+  # The upper limit - Pareto I 
+  p=seq(3.001,400,length.out=250) 
+  g2brup=1/(sqrt(p*(p-2))) 
+  g3brup=(1+p)/(p-3)*2/(sqrt(1-2/p))
+  # The lower limit, corresponding to the Inverted Gamma 
+  g2ibup=seq(0.001,0.999,length.out=250) 
+  g3ibup=4*g2ibup/(1-g2ibup^2) 
+  ##############################################
+  # Lognormal area
+  # Upper limit: Lognormal
+  w=seq(1.01,20,length.out=250)
+  g2log=sqrt(w-1)
+  g3log=(w+2)*sqrt(w-1)
+  # Lower limit - Gamma
+  g2iblow=seq(0,20,length.out=250)
+  g3iblow=2*g2iblow
+  ##############################################
+  # Exponential Area
+  # The upper limit corresponds to the lower limit of the
+  # lognormal area
+  # The lower limit - Bernoulli
+  g2below=seq(0,20,length.out=250)
+  g3below=g2below-1/g2below ##############################################
+  # The Gray area is obtained for free from
+  # the previous lines of code. ##############################################
+  # Normal / Symmetric distribution
+  g2nor=seq(0,20,length.out=250)
+  g3nor=rep(0,250)
+  ##############################################
+  # PLOT
+  # Limits 
+  plot(g2iblow,g3iblow,"l",xlab="CV",ylab="Skewness",main="Discriminant Moment-ratio Plot",xlim=c(0,20),ylim=c(-1,40)) 
+  lines(g2ibup,g3ibup,"l")
+  lines(g2brup,g3brup,"l")
+  lines(g2below,g3below,"l")
+  lines(g2log,g3log,lty=2) # Lognormal
+  lines(g2nor,g3nor,lty=2) # Normal
+  # Strictly Paretian Area 
+  polygon(c(g2ibup,g2brup),c(g3ibup,g3brup),col="green") 
+  points(0,2,pch=1,cex=0.8) # Pareto limit point
+  # Hints for interpretation
+  text(-0.2,20,cex=0.8,srt=90,"Pareto I") 
+  text(1.2,20,cex=0.8,srt=90,"Inverted Gamma")
+  text(2.5,12,cex=0.8,srt=70,"Lognormal") 
+  text(12,21,cex=0.8,srt=23,"Gamma") 
+  text(14,11,cex=0.8,srt=10,"Bernoulli") 
+  text(15,1.5,cex=0.8,"Normal or Symmetric") 
+  points(CV,skew,pch=16,col="red")
+  points(CV,skew,pch=16,col="red")
+  return(c(CV,skew))
+}
+
+moments <- moment_plot(data = data$`Hitting time`)
+
+# Fit a distribution with various method (this is not very informative)
+fitdistrplus::fitdist(data = data$`Hitting time`, distr = "gamma", method = "mme") # parameter est. 
+gamma <- rgamma(n = 10000, shape = 0.2340031181, rate = 0.0003377026) # trying to see if matches the other graphs
+
+### Survival analysis 
+surv_data <- data.frame(Time = data$`Hitting time`, Event = data$Event, row.names = paste0("Sim", 1:nrow(m_times), ""))
+surv_object <- Surv(time = data$`Hitting time`, event = data$Event) 
 surv_fit <- survfit(surv_object ~ 1)
 haz_fit <- bshazard::bshazard(surv_object ~ 1, data = surv_data)
 
@@ -214,7 +355,7 @@ hazard_plot <- plot(x = haz_time, y = hazard, xlab = 'Time', ylab = 'Hazard Rate
                     xlim = c(min(haz_time), max(haz_time)), ylim = c(min(haz_fit$haz), max(haz_fit$haz)))
 
 ## Conditional survival
-fit <- Fwindow(object = surv_fit, width = 10, variance = TRUE, conf.level = 0.95)
+fit <- Fwindow(object = surv_fit, width = 100, variance = TRUE, conf.level = 0.95)
 con_time <- fit$time # the calculated times
 con_death <- fit$Fw # conditional death 
 con_surv <- 1 - con_death # conditional survival; they are mirror images
